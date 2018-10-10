2e148bc8a750eefdc517ebffdc3ec5a4c00ec81d
FeLL1kS <oleg5420@yandex.ru>
2018-10-11 00:37:24 +0300

    -A

---
+++ VKRSL.js
@@ -0,0 +1,17 @@
+async function wait(ms) {
+  return new Promise(r => setTimeout(r, ms));
+};
+async function VKRSL() {
+
+  var like_button = document.querySelector('.like_button_icon');
+  var right_button = document.getElementById('pv_nav_btn_right');
+  while (!like_button.classList.contains('active')){
+    like_button.click();
+    await wait(1000);
+    var event = new Event('mousedown');
+    right_button.dispatchEvent(event);
+    await wait(1000);
+    like_button = document.querySelector('.like_button_icon');
+  }
+
+}
